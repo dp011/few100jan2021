9dda1a5d41c4e4de45da6355eefcc877ea3255a4
Drashti Patel <49107461+dp011@users.noreply.github.com>
2021-01-11 21:47:45 +0000

    Done with day 1

--- spec/demo_with_typescript.spec.ts
+++ spec/demo_with_typescript.spec.ts
@@ -2,6 +2,28 @@
 
 describe('writing specs in typescript', () => {
     it('is easy', () => {
-        expect(true).toBe(false);
+        expect(true).toBe(true);
+    });
+
+    it('can add two numbers', () => {
+        const a = 10;
+        const b = 20;
+
+        const answer = a + b;
+
+        expect(answer).toBe(30);
+    });
+
+});
+describe('this is cool', () => {
+    it('this is another one', () => {
+
+    });
+});
+
+describe('give it a name', () => {
+
+    it('this is a spec', () => {
+
     });
 });
